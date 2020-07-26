d411e871691994dd1b356766f8a1422b6803c234
huoguangxuan <117622536@qq.com>
2020-07-26 23:38:17 +0800

    修改sh脚本

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ npm i && npm run build
 
 # 删除容器
 
-docker rm -f demo1 
+docker rm -f nginx8080 
 
 # 启动容器
 
-docker run -d --restart=on-failure:5 -p 8080:80 --name demo1 -v $PWD/dist:/usr/share/nginx/html nginx
+docker run -d --restart=on-failure:5 -p 8080:80 --name nginx8080 -v $PWD/dist:/usr/share/nginx/html nginx
